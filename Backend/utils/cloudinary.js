9703a5ea2d7ae6eed10e16d867186630e63319ea
princeharshpal <princeharshpal281515@gmail.com>
2025-04-21 11:40:05 +0530

    multer and cloudinary starts here

---
+++ Backend/utils/cloudinary.js
@@ -0,0 +1,22 @@
+import { v2 as cloudinary } from "cloudinary";
+
+const config = async function () {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+};
+
+const uploadResult = await cloudinary.uploader
+  .upload(
+    "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
+    {
+      public_id: "shoes",
+    }
+  )
+  .catch((error) => {
+    console.log(error);
+  });
+
+export default config;
